8b404a621a7d8ed6a0bd267f57c26cbeefddeeef
SphenNocera <noceraa432@gmail.com>
2023-07-19 15:37:22 -0400

    Accepted code

--- 1.two-sum.java
+++ 1.two-sum.java
@@ -7,7 +7,18 @@
 // @lc code=start
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
+        HashMap<Integer, Integer> numList = new HashMap<>();
+        int[] pos = {0, 0}; 
+
+        for(int i = 0; i < nums.length; i++){
+            if(numList.containsKey(target - nums[i])){
+                pos[0] = i;
+                pos[1] = numList.get(target - nums[i]);
+                break;
+            }
+            numList.put(nums[i], i);
+        }
+        return pos;
     }
 }
 // @lc code=end
